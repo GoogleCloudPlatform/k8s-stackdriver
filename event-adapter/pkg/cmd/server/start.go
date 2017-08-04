8f2e44ca882acc8822ac51aef093989c296634fb
Eleonora Rocchi <erocchi@google.com>
2017-08-04 16:37:07 +0200

    Add test to events API

--- event-adapter/pkg/cmd/server/start.go
+++ event-adapter/pkg/cmd/server/start.go
@@ -1,9 +1,12 @@
 /*
 Copyright 2017 The Kubernetes Authors.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -49,17 +52,19 @@ func NewEventsAdapterServerOptions(out, errOut io.Writer) *EventsAdapterServerOp
 	return o
 }
 
-// Validate is setting the server
+// Validate validates EventsAdapterServerOptions. Currently all fields are correctly set in
+// NewsAdapterServerOptions, so this is a no-op.
 func (o EventsAdapterServerOptions) Validate(args []string) error {
 	return nil
 }
 
-// Complete is setting the server
+// Complete fills in any fields not set that are required to have valid data. Currently all fields
+// are set by NewEventsAdapterServerOptions, so this is a no-op.
 func (o *EventsAdapterServerOptions) Complete() error {
 	return nil
 }
 
-// Config sets the server
+// Config returns apiserver.Config object from EventsAdapterServerOptions.
 func (o EventsAdapterServerOptions) Config() (*apiserver.Config, error) {
 	// TODO have a "real" external address (have an AdvertiseAddress?)
 	if err := o.SecureServing.MaybeDefaultWithSelfSignedCerts("localhost", nil, []net.IP{net.ParseIP("127.0.0.1")}); err != nil {
