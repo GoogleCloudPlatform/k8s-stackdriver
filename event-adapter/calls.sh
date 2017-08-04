74535cba8dd36027d870a89fd14ec8de6660e4ad
Eleonora Rocchi <erocchi@google.com>
2017-08-04 16:31:13 +0200

    Register endpoint to list events

--- event-adapter/calls.sh
+++ event-adapter/calls.sh
@@ -2,13 +2,7 @@
 kubectl create -f adapter.yaml
 kubectl proxy & 
 sleep 10
-curl -k localhost:8001
-curl -k localhost:8001//apis/v1events/v1alpha/namespaces/default/events/ere
-curl -k localhost:8001
-curl -k localhost:8001//apis/v1events/v1alpha/namespaces/default/event/ere
-curl -k localhost:8001
-curl -k localhost:8001//apis/v1events/v1alpha/namespces/default/events/ere
-curl -k localhost:8001
-curl -k localhost:8001//namespaces/default/events/ere 
+curl localhost:8001
+curl localhost:8001//apis/v1events/v1alpha1/namespaces/default/events/blah
 kubectl delete -f adapter.yaml
 kill %1
