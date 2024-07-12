56a234d02580255fd51114a565c6dd29e417f34c
Eric Lin <exlin@google.com>
2024-07-12 19:17:57 +0000

    add dependencies for cloud.google.com/go/monitoring

--- prometheus-to-sd/vendor/google.golang.org/api/internal/gensupport/params.go
+++ prometheus-to-sd/vendor/google.golang.org/api/internal/gensupport/params.go
@@ -5,9 +5,11 @@
 package gensupport
 
 import (
+	"net/http"
 	"net/url"
 
 	"google.golang.org/api/googleapi"
+	"google.golang.org/api/internal"
 )
 
 // URLParams is a simplified replacement for url.Values
@@ -37,15 +39,40 @@ func (u URLParams) SetMulti(key string, values []string) {
 	u[key] = values
 }
 
-// Encode encodes the values into ``URL encoded'' form
+// Encode encodes the values into “URL encoded” form
 // ("bar=baz&foo=quux") sorted by key.
 func (u URLParams) Encode() string {
 	return url.Values(u).Encode()
 }
 
-// SetOptions sets the URL params and any additional call options.
+// SetOptions sets the URL params and any additional `CallOption` or
+// `MultiCallOption` passed in.
 func SetOptions(u URLParams, opts ...googleapi.CallOption) {
 	for _, o := range opts {
+		m, ok := o.(googleapi.MultiCallOption)
+		if ok {
+			u.SetMulti(m.GetMulti())
+			continue
+		}
 		u.Set(o.Get())
 	}
+}
+
+// SetHeaders sets common headers for all requests. The keyvals header pairs
+// should have a corresponding value for every key provided. If there is an odd
+// number of keyvals this method will panic.
+func SetHeaders(userAgent, contentType string, userHeaders http.Header, keyvals ...string) http.Header {
+	reqHeaders := make(http.Header)
+	reqHeaders.Set("x-goog-api-client", "gl-go/"+GoVersion()+" gdcl/"+internal.Version)
+	for i := 0; i < len(keyvals); i = i + 2 {
+		reqHeaders.Set(keyvals[i], keyvals[i+1])
+	}
+	reqHeaders.Set("User-Agent", userAgent)
+	if contentType != "" {
+		reqHeaders.Set("Content-Type", contentType)
+	}
+	for k, v := range userHeaders {
+		reqHeaders[k] = v
+	}
+	return reqHeaders
 }
