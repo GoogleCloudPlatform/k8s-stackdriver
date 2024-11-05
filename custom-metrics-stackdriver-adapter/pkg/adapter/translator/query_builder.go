f85193b6687f3345cc4f4c53cb6d8cf9ba2d1bd6
Nabil Dabouz <ndabouz@google.com>
2024-11-05 19:50:21 +0000

    Allow the label resource.labels.container_name instead of all labels starting with resource.labels

--- custom-metrics-stackdriver-adapter/pkg/adapter/translator/query_builder.go
+++ custom-metrics-stackdriver-adapter/pkg/adapter/translator/query_builder.go
@@ -40,8 +40,8 @@ var (
 	allowedExternalMetricsLabelPrefixes  = []string{"metric.labels", "resource.labels", "metadata.system_labels", "metadata.user_labels"}
 	allowedExternalMetricsFullLabelNames = []string{"resource.type", "reducer"}
 	// allowedCustomMetricsLabelPrefixes and allowedCustomMetricsFullLabelNames specify all metric labels allowed for querying
-	allowedCustomMetricsLabelPrefixes  = []string{"metric.labels", "resource.labels"}
-	allowedCustomMetricsFullLabelNames = []string{"reducer"}
+	allowedCustomMetricsLabelPrefixes  = []string{"metric.labels"}
+	allowedCustomMetricsFullLabelNames = []string{"resource.labels.container_name", "reducer"}
 	allowedReducers                    = map[string]bool{
 		"REDUCE_NONE":          true,
 		"REDUCE_MEAN":          true,
